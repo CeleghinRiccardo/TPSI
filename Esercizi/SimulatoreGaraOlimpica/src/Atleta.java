81d8c9f9a064fcb0d81f51434cce6d960aeda114
Riccardo Celeghin <celeghin.riccardo@studenti.salesianisesto.it>
2026-02-06 09:37:39 +0100

    [LAB]

---
+++ Esercizi/SimulatoreGaraOlimpica/src/Atleta.java
@@ -0,0 +1,19 @@
+public class Atleta extends Thread {
+
+    private String name;
+    private long startTime, endTime;
+
+    @Override
+    public void run() {
+        startTime = System.currentTimeMillis();
+        for (int i = 0 ; i <10; i++){
+            System.out.println(name + ": Step "+(i+1));
+        }
+
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getRaceTime(){
+        return endTime - startTime;
+    }
+}
