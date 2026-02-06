41f2a29ea551ba8f5cafab48cd68ce633dcec9ca
Riccardo Celeghin <celeghin.riccardo@studenti.salesianisesto.it>
2026-02-06 10:44:43 +0100

    [LAB]

--- Esercizi/SimulatoreGaraOlimpica/src/Atleta.java
+++ Esercizi/SimulatoreGaraOlimpica/src/Atleta.java
@@ -2,12 +2,24 @@ public class Atleta extends Thread {
 
     private String name;
     private long startTime, endTime;
+    private boolean disqualified;
+
+    public Atleta(String name) {
+        super(name); //setName(name);
+    }
 
     @Override
     public void run() {
         startTime = System.currentTimeMillis();
         for (int i = 0 ; i <10; i++){
-            System.out.println(name + ": Step "+(i+1));
+            System.out.println(getName() + ": Step "+(i+1));
+            try {
+                sleep(300 + (int)(Math.random()*500));
+            } catch (InterruptedException e) {
+                disqualified=true;
+                System.out.println(getName() + "ritirato / qualificato");
+                break;
+            }
         }
 
         endTime = System.currentTimeMillis();
@@ -15,5 +27,9 @@ public class Atleta extends Thread {
 
     public long getRaceTime(){
         return endTime - startTime;
+    }
+
+    public boolean getDisqalified(){
+        return disqualified;
     }
 }
--- Esercizi/SimulatoreGaraOlimpica/src/Main.java
+++ Esercizi/SimulatoreGaraOlimpica/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+
+        Atleta atleta = new Atleta("Andrea");
+        Atleta atleta2 = new Atleta("Luca");
+
+        atleta.start();
+        atleta2.start();
+
+
+
+        //il thread che chiama questo il metodo attende che l'altro thread termini per avanzare
+        try {
+            Thread.sleep(2000);
+            boolean r = new Random().nextBoolean();
+            if(r){
+                atleta.interrupt();
+            }else{
+                atleta2.interrupt();
+            }
+
+            atleta2.join();
+            atleta.join();
+
+            System.out.println(atleta.getName() + " : Tempo gara : " + atleta.getRaceTime());
+            System.out.println(atleta2.getName() + " : Tempo gara : " + atleta2.getRaceTime());
+
+        } catch (InterruptedException e) {
+
+        }
+
+
 
     }
 }
