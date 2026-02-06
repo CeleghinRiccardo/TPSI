d1f12d307c37dc476f8e2a905974c72063e47d4a
Riccardo Celeghin <celeghin.riccardo@studenti.salesianisesto.it>
2026-02-06 10:55:25 +0100

    [LAB]

--- Esercizi/SimulatoreGaraOlimpica/src/Giuria.java
+++ Esercizi/SimulatoreGaraOlimpica/src/Giuria.java
@@ -1,2 +1,18 @@
+import java.lang.classfile.AttributedElement;
+import java.util.ArrayList;
+
 public class Giuria {
+
+    private ArrayList<Atleta> atletiqualificati;
+    private ArrayList<Atleta> atletisqualificati;
+
+    public Giuria(){
+        atletiqualificati = new ArrayList<>();
+        atletisqualificati = new ArrayList<>();
+    }
+
+    public void push(){
+        
+    }
+
 }
