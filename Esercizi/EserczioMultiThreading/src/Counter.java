0fdae72bc56358615008044cd7dda49cb96e98af
Riccardo Celeghin <celeghin.riccardo@studenti.salesianisesto.it>
2026-02-13 10:43:22 +0100

    [LAB]

---
+++ Esercizi/EserczioMultiThreading/src/Counter.java
@@ -0,0 +1,14 @@
+public class Counter {
+    private long counter;
+
+    public void add(long value){
+        //this.counter+=value;
+        long tmp = this.counter;
+        tmp = tmp +value;
+        this.counter = tmp;
+    }
+
+    public long getCounter(){
+        return counter;
+    }
+}
---
+++ Esercizi/EserczioMultiThreading/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        AdderThread adderThread = new AdderThread();
+        AdderThread adderThread2 = new AdderThread();
+
+        adderThread.start();
+        adderThread2.start();
+        adderThread.join();
+        adderThread2.join();
+
+        System.out.println("Counter: " + AdderThread.counter.getCounter());
+    }
+}
